b67f4ff8757e29069754a327ace9afe979f09be8
godspowerufot <ufotgodspower8@gmail.com>
2024-09-15 11:05:24 +0100

    changes

--- app/(root)/Registration/page.tsx
+++ app/(root)/Registration/page.tsx
@@ -148,7 +148,7 @@ const Register = () => {
                       className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
                       type="text"
                       name="referred_by"
-                      placeholder="Referral ID"
+                      placeholder="Referral "
                       value={formData.referred_by}
                       onChange={handleChange}
                     />
