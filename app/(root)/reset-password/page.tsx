58a68893d3d29cfd44d29e1abf66d9980e9a5bc6
godspowerufot <ufotgodspower8@gmail.com>
2024-09-20 17:18:56 +0100

    hot referraltwo

--- app/(root)/reset-password/page.tsx
+++ app/(root)/reset-password/page.tsx
@@ -97,7 +97,7 @@ setLoading(false)
           className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
           disabled={loading}
         >
-          {loading ? "Resetting Password..." : "Reset Password"}
+          {loading ? "Resetting Passwords..." : "Reset Password"}
         </button>
       </form>
     </div>
